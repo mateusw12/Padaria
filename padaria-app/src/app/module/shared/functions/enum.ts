dc2b3f9ec229c20946db40cf1ad7a0fd29bbab99
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2022-02-26 12:00:59 -0300

    Implementa funções relacionadas a enum

---
+++ padaria-app/src/app/module/shared/functions/enum.ts
@@ -0,0 +1,26 @@
+interface Enum {
+  value: number;
+  description: string;
+}
+
+export function enumToArray(enumeration: Map<any, string>): Enum[] {
+  const enumItems: Enum[] = [];
+  for (const item of enumeration) {
+    enumItems.push({
+      value: item[0],
+      description: item[1],
+    });
+  }
+  return enumItems.filter((el) => el.value !== 0);
+}
+
+export function EnumDescription(
+  enumeration: Map<any, string>,
+  value: number
+): string {
+  let description = '';
+  for (const item of enumeration) {
+    if (item[0] === value) description = item[1] as string;
+  }
+  return description;
+}
