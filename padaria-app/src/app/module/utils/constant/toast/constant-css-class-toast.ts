f7d1515d175083345f504077d708d0b7be7a41b8
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2022-04-09 13:26:53 -0300

    Implementa diretiva do grid sync fusion

--- padaria-app/src/app/module/utils/constant/toast/constant-css-class-toast.ts
+++
@@ -1,4 +0,0 @@
-export const SUCCESS_CSS_CLASS = 'e-toast-success';
-export const WARNING_CSS_CLASS = 'e-toast-warning';
-export const ERROR_CSS_CLASS = 'e-toast-danger';
-export const INFO_CSS_CLASS = 'e-toast-info';
