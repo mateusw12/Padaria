b19589c18229a6d6fbb08f30c82253c4dd5ec4bc
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2022-10-16 22:01:41 -0300

    Implementa novas funções utils

--- padaria-app/src/app/module/utils/functions/index.ts
+++ padaria-app/src/app/module/utils/functions/index.ts
@@ -1,3 +1,4 @@
-export * from './enum';
 export * from './dictionary';
 export * from './row-data-target';
+export * from './get-file-extension';
+export * from './tranform-array-buffer-base-64';
--- padaria-app/src/app/module/utils/interfaces/index.ts
+++ padaria-app/src/app/module/utils/interfaces/index.ts
@@ -1,3 +1,5 @@
+export * from './app-file';
 export * from './enum-interface';
 export * from './state-interface';
 export * from './tab-interface';
+
