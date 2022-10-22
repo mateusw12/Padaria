6da34311ec0bb4448056c8668b7931d5a4b07a18
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2022-10-22 16:09:50 -0300

    Implementa bradCrumb nas rotas

--- padaria-app/src/app/module/routes/src/routes.ts
+++ padaria-app/src/app/module/routes/src/routes.ts
@@ -7,88 +7,88 @@ export const routes: Routes = [
   {
     path: 'menu',
     component: MenuComponent,
-    data: { pageTitle: 'Menu' },
+    data: { pageTitle: 'Menu', breadcrumb: 'Menu' },
     children: [
       {
         path: 'home',
-        data: { pageTitle: 'Home' },
+        data: { pageTitle: 'Home', breadcrumb: 'Home' },
         loadChildren: pages.homeRegistration,
       },
       {
         path: 'registrations',
-        data: { pageTitle: 'Cadastros' },
+        data: { pageTitle: 'Cadastros', breadcrumb: 'Cadastros' },
         children: [
           {
             path: 'jobs',
-            data: { pageTitle: 'Cargos' },
+            data: { pageTitle: 'Cargos', breadcrumb: 'Cargos' },
             loadChildren: pages.jobRegistration,
           },
           {
             path: 'departaments',
-            data: { pageTitle: 'Departamentos' },
+            data: { pageTitle: 'Departamentos', breadcrumb: 'Departamentos' },
             loadChildren: pages.departamentRegistration,
           },
           {
             path: 'products',
-            data: { pageTitle: 'Produtos' },
+            data: { pageTitle: 'Produtos', breadcrumb: 'Produtos' },
             loadChildren: pages.productRegistration,
           },
           {
             path: 'employees',
-            data: { pageTitle: 'Funcionários' },
+            data: { pageTitle: 'Funcionários', breadcrumb: 'Funcionários' },
             loadChildren: pages.employeeRegistration,
           },
           {
             path: 'suppliers',
-            data: { pageTitle: 'Fornecedores' },
+            data: { pageTitle: 'Fornecedores', breadcrumb: 'Fornecedores' },
             loadChildren: pages.supplierRegistration,
           },
           {
             path: 'note-types',
-            data: { pageTitle: 'Tipos de Nota' },
+            data: { pageTitle: 'Tipos de Nota', breadcrumb: 'Tipos de Nota' },
             loadChildren: pages.noteTypeRegistration,
           },
           {
             path: 'manufacturers',
-            data: { pageTitle: 'Fabricantes' },
+            data: { pageTitle: 'Fabricantes', breadcrumb: 'Fabricantes' },
             loadChildren: pages.manufacturerRegistration,
           },
         ],
       },
       {
         path: 'queries',
-        data: { pageTitle: 'Consultas' },
+        data: { pageTitle: 'Consultas', breadcrumb: 'Consultas' },
         children: [
           {
             path: 'employee-query',
-            data: { pageTitle: 'Funcionários' },
+            data: { pageTitle: 'Funcionários', breadcrumb: 'Funcionários' },
             loadChildren: pages.employeeQueryRegistration,
           },
           {
             path: 'products-query',
-            data: { pageTitle: 'Produtos' },
+            data: { pageTitle: 'Produtos', breadcrumb: 'Produtos' },
             loadChildren: pages.productQueryRegistration,
           },
         ],
       },
       {
         path: 'inventory',
-        data: { pageTitle: 'Estoque' },
+        data: { pageTitle: 'Estoque', breadcrumb: 'Estoque' },
         loadChildren: pages.inventoryRegistration,
       },
       {
         path: 'information',
-        data: { pageTitle: 'Sobre' },
+        data: { pageTitle: 'Sobre', breadcrumb: 'Sobre' },
         loadChildren: pages.informationRegistration,
       },
       {
         path: 'license',
-        data: { pageTitle: 'Licenças' },
+        data: { pageTitle: 'Licenças', breadcrumb: 'Licenças' },
         loadChildren: pages.licenseRegistration,
       },
       {
         path: 'purchase-control',
-        data: { pageTitle: 'Controle Compras' },
+        data: { pageTitle: 'Controle Compras', breadcrumb: 'Controle de Compras' },
         loadChildren: pages.purchaseControlRegistration,
       },
     ],
