167618ea1565748c6d7969fa34e47202c177aa21
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2022-12-02 22:52:41 -0300

    Implementa nome do produto dinâmico na página de informações gerais Implementa nome do produto dinâmico na página de informações gerais Implementa nome dinâmico na página de informações sobre o produto

--- padaria-app/src/app/module/pages/information/src/information.component.ts
+++ padaria-app/src/app/module/pages/information/src/information.component.ts
@@ -1,8 +1,40 @@
+import { untilDestroyed } from '@module/utils/common';
+import { OnInit, OnDestroy } from '@angular/core';
 import { Component } from '@angular/core';
+import { SettingRepository } from '@module/repository';
+import { ErrorHandler } from '@module/utils/services';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
   styleUrls: ['./information.component.scss'],
 })
-export class InformationComponent {}
+export class InformationComponent implements OnInit, OnDestroy {
+  title: string = '';
+
+  constructor(
+    private settingsRepository: SettingRepository,
+    private errorHandler: ErrorHandler
+  ) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  ngOnDestroy(): void {}
+
+  private loadData(): void {
+    this.settingsRepository
+      .find()
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        (settings) => (this.title = settings.name),
+        (error) => this.handleError(error)
+      );
+  }
+
+  private handleError(error: unknown): void {
+    this.errorHandler.present(error);
+  }
+  
+}
