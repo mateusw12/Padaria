1fe202d88c83e4ac08ac3eea39c35caff6b18248
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-28 20:28:39 -0300

    Remove exceções não utilizadas

--- padaria-api/src/main/java/com/padaria/exceptions/EntityInternalServerErrorException.java
+++
@@ -1,9 +0,0 @@
-package com.padaria.exceptions;
-
-public class EntityInternalServerErrorException extends RuntimeException {
-
-    public EntityInternalServerErrorException(String msg){
-        super(msg);
-    }
-
-}
